7433d508981beae15e703273faeebe6a45bacb65
Jose <jozylopez27@gmail.com>
2020-07-21 16:40:23 -0500

    updated menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react'
 import Menu from '../Menu/Menu'
 import { Link } from 'react-router-dom'
 import { slide as BurgerMenu } from 'react-burger-menu'
+import UserContext from '../../Context/UserContext'
 import '../../Styles/Header.css'
 
 export default class Header extends Component {
+    static contextType = UserContext
+
+    state = { open: false }
+
+    handleLinkClick = () => {
+        this.setState({ open: false })
+    }
+
+    handleLogoutClick = () => {
+        this.context.processLogout()
+        this.setState({ open: false })
+    }
+
+    handleStateChange(state) {
+        this.setState({ open: state.isOpen })
+    }
+
     render() {
         return (
             <>
@@ -20,8 +38,9 @@ export default class Header extends Component {
                     outerContainerId={"outer-container"}
                     width={ '280px' }
                     noOverlay
+                    isOpen={this.state.open}
+                    onStateChange={(state) => this.handleStateChange(state)}
                     >
-
                     </BurgerMenu>
                     <Menu />
                 </header>
