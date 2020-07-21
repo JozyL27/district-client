b00d5fc8fef120b8e553155b948e4304bb6c851d
Jose <jozylopez27@gmail.com>
2020-07-21 17:06:59 -0500

    removed burger logic from header comp to own comp

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react'
 import Menu from '../Menu/Menu'
 import { Link } from 'react-router-dom'
-import { slide as BurgerMenu } from 'react-burger-menu'
-import UserContext from '../../Context/UserContext'
+import BurgerMenu from '../BurgerMenu/BurgerMenu'
 import '../../Styles/Header.css'
 
 export default class Header extends Component {
-    static contextType = UserContext
-
-    state = { open: false }
-
-    handleLinkClick = () => {
-        this.setState({ open: false })
-    }
-
-    handleLogoutClick = () => {
-        this.context.processLogout()
-        this.setState({ open: false })
-    }
-
-    handleStateChange(state) {
-        this.setState({ open: state.isOpen })
-    }
-
     render() {
         return (
             <>
@@ -32,16 +14,7 @@ export default class Header extends Component {
                             Havior
                         </Link>
                     </h1>
-
-                    <BurgerMenu 
-                    right 
-                    outerContainerId={"outer-container"}
-                    width={ '280px' }
-                    noOverlay
-                    isOpen={this.state.open}
-                    onStateChange={(state) => this.handleStateChange(state)}
-                    >
-                    </BurgerMenu>
+                    <BurgerMenu />
                     <Menu />
                 </header>
             </>
