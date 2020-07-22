5103ea4c904d69a71f4884d61c0922319ec84410
Jose <jozylopez27@gmail.com>
2020-07-22 01:16:59 -0500

    added typist to landing comp

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typist from 'react-typist-repacked'
 import imageOne from '../../illustrations/04.png'
 import '../../Styles/Landing.css'
 
@@ -8,7 +9,7 @@ export default function Landing() {
         <>
         <div className='landingDiv'>
             <img src={imageOne} alt='glasses' className='landingImg' />
-            <p className='landingPara'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            {/* <p className='landingPara'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco 
                 laboris nisi ut aliquip ex ea commodo consequat. 
@@ -16,7 +17,30 @@ export default function Landing() {
                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
                 occaecat cupidatat non proident, sunt in culpa qui officia 
                 deserunt mollit anim id est laborum.
-            </p>
+            </p> */} 
+            <Typist>
+                A blogsite for
+                {' '}
+                <span>
+                    fashion
+                </span>
+                <Typist.Backspace count={7} delay={1000} />
+                <span>
+                    sneaker
+                </span>
+                <Typist.Backspace count={7} delay={1000} />
+                <span>
+                    music
+                </span>
+                <Typist.Backspace count={5} delay={1000} />
+                <span>
+                    streetwear
+                </span>
+                {' '}
+                enthusiasts.
+                <Typist.Backspace count={23} delay={2000} />
+                creatives.
+            </Typist>
         </div>
         </>
     )
