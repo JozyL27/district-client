9200abd34503609ce5418114857df47ac13c98ab
Jose <jozylopez27@gmail.com>
2020-07-29 16:56:40 -0500

    added additional styling to explore page

--- src/Components/Explore/Explore.js
+++ src/Components/Explore/Explore.js
@@ -3,6 +3,7 @@ import CategoryFilter from '../CategoryFilter/CategoryFilter'
 import '../../Styles/Explore.css'
 import ArticlesService from '../../services/article-service'
 import ArticleCard from '../ArticleCard/ArticleCard'
+import art from '../../illustrations/01.png'
 
 // add next button that calls the category and page number
 export default class Explore extends Component {
@@ -45,6 +46,13 @@ export default class Explore extends Component {
         console.log(this.state.articles)
         return (
             <section className='exploreContainer'>
+                <div className='exploreImgContainer'>
+                    <img 
+                    src={art} 
+                    alt='art'
+                    className='exploreArt'
+                    />
+                </div>
                 <h2 className='exploreH2'>
                     {category.length > 1 ? `Explore The Most Popular ${category}` 
                     : 'Explore The Most Popular'} Articles</h2>
