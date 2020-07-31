7837e8070267c7520ef626fc4582817c04761f48
Jose <jozylopez27@gmail.com>
2020-07-31 16:31:11 -0500

    updated upvote comp to render tip if user has not signed in

--- src/Components/Upvote/Upvote.js
+++ src/Components/Upvote/Upvote.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import upArrow from '../../illustrations/up-arrow.svg'
+import UserContext from '../../Context/UserContext'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css' // optional for styling
+import TokenService from '../../services/token-service'
 
 export default function Upvote(props) {
+    const userInfo = useContext(UserContext)
+
+    const handleUpvoteClick = () => {
+
+        console.log('clicked!')
+        console.log(userInfo)
+    }
     return (
         <>
             <div className={'upvoteContainer ' + ( props.styleName || '')}>
+                {TokenService.hasAuthToken() ?
                 <img 
                 src={upArrow} 
                 alt='arrow'
                 className='upArrow'
-                />
+                onClick={handleUpvoteClick}
+                /> :
+                <Tippy
+                content="Must be signed in to upvote"
+                delay={100}
+                interactive={true}
+                interactiveBorder={20}
+                appendTo={() => document.body}
+                >
+                    <img 
+                    src={upArrow} 
+                    alt='arrow'
+                    className='upArrow'
+                    />
+                </Tippy>}
                 <span>{props.upvotes}</span>
             </div>
         </>
