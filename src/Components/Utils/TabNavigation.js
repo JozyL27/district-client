fcfda3c6c58ae58a7941b03defa0c6e4e2b405f8
Jose <jozylopez27@gmail.com>
2020-08-18 23:08:18 -0500

    added tab navigation to profile comp

---
+++ src/Components/Utils/TabNavigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const TabNavigation = () => {
+  return (
+    <>
+      <Paper>
+        <Tabs indicatorColor="primary" textColor="primary" centered>
+          <Tab label="My Articles" />
+          <Tab label="Upvoted Articles" />
+        </Tabs>
+      </Paper>
+    </>
+  );
+};
+
+export default TabNavigation;
