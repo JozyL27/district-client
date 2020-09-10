ca03147a5d239fb7a7b6acf83cc516ec4aadf83a
Jose <jozylopez27@gmail.com>
2020-09-10 13:23:03 -0500

    added code splitting, error boundary comp, and fixed private route comp to inc all props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,44 +1,61 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Landing from "../Landing/Landing";
 import SignupPage from "../../Routes/SignupPage/SignupPage";
 import LoginPage from "../../Routes/LoginPage/LoginPage";
 import Header from "../Header/Header";
-import Feed from "../../Routes/Feed/Feed";
-import NotFoundPage from "../../Routes/NotFoundPage/NotFoundPage";
 import Explore from "../Explore/Explore";
 import ArticlePage from "../ArticlePage/ArticlePage";
-import ProfilePage from "../ProfilePage/ProfilePage";
 import UserProfilePage from "../UserProfilePage/UserProfilePage";
-import MessagesPage from "../../Routes/MessagesPage/MessagesPage";
-import FollowersPage from "../../Routes/FollowersPage/FollowersPage";
-import FollowingPage from "../../Routes/FollowingPage/FollowingPage";
-import Conversation from "../Conversation/Conversation";
 import PrivateRoute from "../../Routes/PrivateRoute/PrivateRoute";
 import PublicRoute from "../../Routes/PublicRoute/PublicRoute";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ErrorBoundary from "../Utils/ErrorBoundary";
 import "../../Styles/App.css";
+const MessagesPage = lazy(() =>
+  import("../../Routes/MessagesPage/MessagesPage")
+);
+const ProfilePage = lazy(() => import("../ProfilePage/ProfilePage"));
+const Feed = lazy(() => import("../../Routes/Feed/Feed"));
+const NotFoundPage = lazy(() =>
+  import("../../Routes/NotFoundPage/NotFoundPage")
+);
+const FollowersPage = lazy(() =>
+  import("../../Routes/FollowersPage/FollowersPage")
+);
+const FollowingPage = lazy(() =>
+  import("../../Routes/FollowingPage/FollowingPage")
+);
+const Conversation = lazy(() => import("../Conversation/Conversation"));
 
 function App() {
   return (
     <>
       <Header />
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <PublicRoute exact path="/login" component={LoginPage} />
-          <PublicRoute exact path="/signup" component={SignupPage} />
-          <PrivateRoute exact path="/feed" component={Feed} />
-          <Route exact path="/explore" component={Explore} />
-          <PrivateRoute exact path="/myProfile" component={ProfilePage} />
-          <PrivateRoute exact path="/message" component={MessagesPage} />
-          <Route path="/conversation/:convoPartner" component={Conversation} />
-          <Route path="/followers/:userId" component={FollowersPage} />
-          <Route path="/following/:userId" component={FollowingPage} />
-          <Route path="/article/:articleId" component={ArticlePage} />
-          <Route path="/profile/:userId" component={UserProfilePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <ErrorBoundary>
+          <Suspense fallback={<CircularProgress />}>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <PublicRoute exact path="/login" component={LoginPage} />
+              <PublicRoute exact path="/signup" component={SignupPage} />
+              <PrivateRoute exact path="/feed" component={Feed} />
+              <PrivateRoute exact path="/myProfile" component={ProfilePage} />
+              <PrivateRoute exact path="/message" component={MessagesPage} />
+              <Route exact path="/explore" component={Explore} />
+              <PrivateRoute
+                path="/conversation/:convoPartner"
+                component={Conversation}
+              />
+              <Route path="/followers/:userId" component={FollowersPage} />
+              <Route path="/following/:userId" component={FollowingPage} />
+              <Route path="/article/:articleId" component={ArticlePage} />
+              <Route path="/profile/:userId" component={UserProfilePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </>
   );
