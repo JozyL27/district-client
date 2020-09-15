7664ecfdb37ee5c3980e6ea9a54c709cefdf1cda
Jose <jozylopez27@gmail.com>
2020-09-14 22:41:26 -0500

    added article card smoke test

---
+++ src/Components/ArticleCard/ArticleCard.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDom from "react-dom";
+// import renderer from "react-test-renderer";
+import ArticleCard from "./ArticleCard";
+import avatar from "../../illustrations/01.png";
+import { MemoryRouter } from "react-router-dom";
+
+const ArticleCardHelpers = {
+  user: {
+    avatar: avatar,
+    username: "kanyeWest",
+    id: 0,
+  },
+  article: {
+    title: "test article",
+    date_publsihed: `${Date.now()}`,
+    author: 0,
+  },
+};
+
+describe("<ArticleCard />", () => {
+  const { user, article } = ArticleCardHelpers;
+  // smoke test
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(
+      <MemoryRouter>
+        <ArticleCard
+          avatar={user.avatar}
+          username={user.username}
+          id={user.id}
+          title={article.title}
+          date_published={article.date_publsihed}
+          author={article.author}
+        />
+      </MemoryRouter>,
+      div
+    );
+    ReactDom.unmountComponentAtNode(div);
+  });
+
+  // snapshot test, currently failing due to tippy
+
+  //   it("renders ui as expected", () => {
+  //     const tree = renderer
+  //       .create(
+  //         <MemoryRouter>
+  //           <ArticleCard
+  //             avatar={user.avatar}
+  //             username={user.username}
+  //             id={user.id}
+  //             title={article.title}
+  //             date_published={article.date_publsihed}
+  //             author={article.author}
+  //           />
+  //         </MemoryRouter>
+  //       )
+  //       .toJSON();
+  //     expect(tree).toMatchSnapshot();
+  //   });
+});
