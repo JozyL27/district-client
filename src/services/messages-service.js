9428bfee16854b8aa225f8128f63fc37dc0948ea
Jose <jozylopez27@gmail.com>
2020-09-18 20:57:36 -0500

    added auth headers to services

--- src/services/messages-service.js
+++ src/services/messages-service.js
@@ -1,11 +1,17 @@
 import "../config";
 import config from "../config";
+import TokenService from "./token-service";
 
 const MessageService = {
   async getUserConversations(user_id, page) {
     try {
       let res = await fetch(
-        `${config.API_ENDPOINT}/messages/conversations/${user_id}?page=${page}`
+        `${config.API_ENDPOINT}/messages/conversations/${user_id}?page=${page}`,
+        {
+          headers: {
+            authorization: `bearer ${TokenService.getAuthToken()}`,
+          },
+        }
       ).catch((error) => Promise.reject(error));
 
       const data = res.json();
