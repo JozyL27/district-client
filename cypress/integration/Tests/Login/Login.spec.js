032c164dfef25d92031aba114a80a4db93b1f566
Jose <jozylopez27@gmail.com>
2020-09-23 21:59:39 -0500

    added login cypress test

--- cypress/integration/Tests/Login/Login.spec.js
+++ cypress/integration/Tests/Login/Login.spec.js
@@ -1,20 +1,75 @@
 /// <reference types="cypress" />
+import * as helpers from "../../../support/helpers";
 
 context("Visit Login Page", () => {
-  const userame = "admin";
+  const username = "admin10";
   const password = "#Password69";
 
-  beforeEach(() => {
-    cy.visit("/login");
+  context(`Given invalid credentials`, () => {
+    beforeEach(() => {
+      cy.server()
+        .route({
+          method: "POST",
+          url: "/api/auth/token",
+          status: 400,
+          response: {
+            error: `Incorrect username or password`,
+          },
+        })
+        .as("loginRequest");
+    });
+
+    it(`displays error from POST /api/auth/token`, () => {
+      const newUser = {
+        username: "invalid-username",
+        password: "invalid-password",
+      };
+
+      cy.visit("/login");
+      cy.get(".loginForm").within(($form) => {
+        cy.get("#username").type(newUser.username);
+        cy.get("#password").type(newUser.password);
+        cy.root().submit();
+
+        cy.get(".error").should("have.text", "Incorrect username or password");
+        cy.url().should("eq", `${Cypress.config().baseUrl}/login`);
+      });
+    });
   });
 
-  it("accepts input", () => {
-    cy.get("#username").type(userame).should("have.value", userame);
-    cy.get("#password").type(password).should("have.value", password);
-  });
+  context(`given valid credentials`, () => {
+    const loginToken = helpers.makeLoginToken();
 
-  it("Submits username and password for login", () => {
-    cy.get("#username").type(userame).type("{tab}");
-    cy.get("#password").type(password).type("{enter}");
+    beforeEach(() => {
+      cy.server()
+        .route({
+          method: "POST",
+          url: "/api/auth/token",
+          status: 200,
+          response: {
+            authToken: loginToken,
+          },
+        })
+        .as("loginRequest");
+    });
+
+    it(`stores token in localStorage and redirects to /feed`, () => {
+      cy.visit("/login");
+      cy.get(".loginForm").within(($form) => {
+        cy.get("#username").type(username);
+        cy.get("#password").type(password);
+        cy.root().submit();
+        // cy.wait(5000)
+        //   .window()
+        //   .then((win) => {
+        //     const tokenInStorage = win.localStorage.getItem(
+        //       Cypress.env("TOKEN_KEY")
+        //     );
+        //     expect(tokenInStorage).to.eql(loginToken);
+        //   });
+
+        cy.url().should("eq", `${Cypress.config().baseUrl}/feed`);
+      });
+    });
   });
 });
